031dd107a0c227ab68d6e60c18df90c2f64fe9e6
Jez Halford <jezhalford@gmail.com>
2013-05-30 21:44:29 +0100

    Added cocktail finding logic

--- public/index.php
+++ public/index.php
@@ -40,10 +40,32 @@ $app->get('/', function () use ($app) {
 });
 
 $app->get('ingredient-lookup', function (Request $request) use ($app) {
-    $sql = "SELECT * FROM `ingredients` WHERE `name` LIKE CONCAT(?, '%')";
-    $ingredients = $app['db']->fetchAll($sql, array($request->get('q')));
+    $sql = "SELECT `name` AS 'label', `name` AS 'value', `id` FROM `ingredients` WHERE `name` LIKE CONCAT('%', ?, '%')";
+    $ingredients = $app['db']->fetchAll($sql, array($request->get('term')));
 
     return new Response(json_encode($ingredients), 200, array('Content-Type' => 'application/json'));
+
+});
+
+$app->get('recipe-lookup', function (Request $request) use ($app) {
+
+    $ingredients = explode(',', $request->get('ingredients'));
+
+    $placeholders = array_fill(0, count($ingredients), '?');
+
+    $sql = sprintf("SELECT r.name as recipe, r.id as id, COUNT(i.id) AS ingredients,
+            (SELECT COUNT(sri.ingredient_id) FROM recipes_ingredients sri WHERE sri.recipe_id = r.id) as c
+            FROM
+            recipes r
+            LEFT JOIN recipes_ingredients ri ON ri.recipe_id = r.id
+            LEFT JOIN ingredients i ON ri.ingredient_id = i.id
+            WHERE (%s)
+            GROUP BY r.id HAVING ingredients = c
+            ORDER BY i.id;", 'i.id = ' . implode(' OR i.id = ', $placeholders));
+
+    $recipes = $app['db']->fetchAll($sql, $ingredients);
+
+    return new Response(json_encode($recipes), 200, array('Content-Type' => 'application/json'));
 
 });
 
